65fae56ee447756dede9f58291bda2c8218a2755
IgorSantosVital <igoralexandre2@hotmail.com>
2017-10-17 20:06:28 -0200

    atualizado

--- saida.php
+++ saida.php
@@ -4,34 +4,33 @@ $numerode1 = $_POST["num1"];
 $numeroate2 = $_POST["num2"];
 $multiplicandode1 = $_POST["multi1"];
 $multiplicandoate2 = $_POST["multi2"];
+?>
+
+<!DOCTYPE html>
+    <html>
+    <link rel='stylesheet' href='css/table.css'>
+    <br>
+<?php for($i=$numerode1; $i<=$numeroate2; $i++){ ?>
 
 
-
-for($i=$numerode1; $i<=$numeroate2; $i++) {
-
-    echo "<br>
-    <caption>Tabuado do $i</caption>
-        <table border='1'>
-        <thead class='cabecalho'>
+        <table>
+        <caption><h4>Tabuado do <?php echo $i ?></h4></caption>
+        <thead>
             <th>Fórmula</th>
             <th>Valor</th>
-        </thead>";
-
-    for ($multi3=$multiplicandode1; $multi3<=$multiplicandoate2; $multi3++){
+        </thead>
+        
+<tbody>
+    <?php for ($multi3=$multiplicandode1; $multi3<=$multiplicandoate2; $multi3++) { ?>
     
-        $resultado = ($i * $multi3);
-        // echo "$i x $multi3 = $resultado <br>";
-    
-
-
-    echo "<table border='1'>
-        <tbody>
             <tr>
-                <td>$i x $multi3</td>
-                <td>$resultado</td>
+                <td><?php echo $i.'x'.$multi3 ?> </td>
+                <td><?php echo ($i * $multi3) ?> </td>
             </tr>
-        </tbody>
-        </table>";
-    }
-}
-?>
+        
+    <?php } ?>
+    </tbody>
+    </table>
+<?php } ?>
+
+</html>
